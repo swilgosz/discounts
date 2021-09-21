c515f0990284d8e57ad58779b4ab1022fd00b7d7
Sebastian Wilgosz <sebastian@driggl.com>
2021-09-21 22:49:15 +0200

    Add products factory for easier testing

---
+++ spec/factories/products.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :product do
+    trait :green_tea do
+      code { "GT1" }
+      price  { "3.11" }
+      name { "Green Tea" }
+    end
+
+    trait :strawberries do
+      name { "Strawberies" }
+      code { "STR1" }
+      price { 5.00 }
+    end
+
+    trait :coffee do
+      name { "Coffee" }
+      code { "CF1" }
+      price { 11.23 }
+    end
+  end
+end
